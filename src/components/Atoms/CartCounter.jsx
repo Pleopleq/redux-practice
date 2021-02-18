47443cca6ac1660ca037f0771e4925579d2e7018
Felix Anducho <felix.anducho74@gmail.com>
2021-02-18 13:33:22 -0600

    Add initial files

---
+++ src/components/Atoms/CartCounter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CartCounter = () => (
+    <>
+        <li>
+            <button className="button tiny ghost">Carrito: 5</button>
+        </li>
+    </>
+)
+
+
+export default CartCounter
